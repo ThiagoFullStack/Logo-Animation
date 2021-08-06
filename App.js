5df9f90ab370426d24cd3ca95bbec3eaf3b24417
ThiagoFullStack <thiagobarcos@yahoo.com.br>
2021-08-06 06:38:06 -0300

    initial version.

--- App.js
+++ App.js
@@ -1,21 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler'
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, KeyboardAvoidingView, Image, TextInput, TouchableOpacity, Text, StyleSheet, Animated} from 'react-native';
+import * as Animatable from 'react-native-animatable';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+
+return (
+<KeyboardAvoidingView style={styles.background}>
+ <View style={styles.containerLogo}>
+   <Image 
+    source={require('./src/assets/logo.png')}
+   />
+  
+ </View>
+ <Animatable.View 
+ style={styles.container}
+ 
+ animation= 'bounceInUp'
+ useNativeDriver
+ duration={2000}
+>
+  <TextInput
+  style={styles.input}
+  placeholder='Email'
+  autoCorrect={false}
+  onChange={()=>{}}
+  />
+
+<TextInput
+  style={styles.input}
+  placeholder='Senha'
+  autoCorrect={false}
+  onChange={()=>{}}
+  />
+
+  <TouchableOpacity style={styles.btnSubmit}>
+    <Text style={styles.submitText}>Acessar</Text>
+  </TouchableOpacity>
+
+  <TouchableOpacity style={styles.btnRegister}>
+    <Text style={styles.registerText}>Criar conta gratuita</Text>
+  </TouchableOpacity>
+
+ </Animatable.View>
+</KeyboardAvoidingView>
+);
 }
 
+
 const styles = StyleSheet.create({
-  container: {
+  background:{
+flex: 1,
+backgroundColor: '#191919',
+alignItems: 'center',
+ justifyContent: 'center'
+
+  },
+  containerLogo:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  container:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+    paddingBottom: 50
+  },
+  input:{
+    backgroundColor: '#fff',
+    width: '90%',
+    marginBottom: 15,
+    color: '#222',
+    fontSize: 17,
+    borderRadius: 7,
+    padding: 10,
+  },
+  btnSubmit:{
+    backgroundColor: '#35AAFF',
+    width: '90%',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7
+  },
+  submitText:{
+    color: '#fff',
+    fontSize: 18
+  },
+  btnRegister:{
+    marginTop: 10,
+  },
+  registerText:{
+    color: '#FFF'
+  }
+
+})
+
